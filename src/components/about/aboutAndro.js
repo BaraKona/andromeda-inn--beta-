bab50417edc16ed006e1e9d5f87f6b92bc27fb72
Bara Konateh <bkonateh1@gmail.com>
2021-10-01 11:06:33 +0100

    aboutPage work

--- src/components/about/aboutAndro.js
+++ src/components/about/aboutAndro.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import '../css/aboutAndro.css'
 
 export default function aboutAndro() {
     return (
-        <div className = "aboutAndro">
-            <div className = "androContainer">
+        <section className = "aboutAndro">
+            <div className = "aboutFrame">
+                <div className = "androContainer">
+                    <div className = "title1">
+                        <h1> What is Andromeda Inn ?</h1>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div className = "flex_items">
+                <div className = "col"> 1</div>
+                <div className = "col"> 2</div>
+                <div className = "col"> 3</div>
+                <div className = "col"> 4</div>
+                <div className = "col"> 5</div>
+            </div>
+
+        </section>
     )
 }
 
---
+++ src/components/about/aboutMe.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../css/aboutAndro.css'
+export default function aboutMe() {
+    return (
+        <div className = "aboutMe">
+            <div className = "aboutMeContainer">
+                <h1> HEEEEEELLAHSOK</h1>
+            </div>
+        </div>
+    )
+}
