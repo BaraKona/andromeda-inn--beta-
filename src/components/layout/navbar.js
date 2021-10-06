2cc85cc325e32e427efddbf3ea086d66b1eccb60
Bara Konateh <bkonateh1@gmail.com>
2021-10-06 16:47:32 +0100

    Implemented Mobile navbar

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,65 +1,71 @@
 import '../css/navbar.css'
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
 import img from '../../images/icons/magicbook2.svg'
- const Navbar = () => {
+
+
+const Navbar = () => {
   const { currentUser } = useAuth();
   const { logout } = useAuth();
+  const [mobileMenu, setMobileMenu] = useState("");
 
+  const switchMobile = () =>{
+    setMobileMenu( mobileMenu === "" ? "active" : "" )
+  }
   function navlog(){
     if (currentUser !== null){
           return <>
-            <li class="nav-item">
-              <Link class="nav-link active" to="/about">About</Link>
+            <li className={`nav-item ${mobileMenu}`}>
+              <Link className="nav-link active" to="/about">About</Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/">Home</Link>
+            <li className={`nav-item ${mobileMenu}`}>
+              <Link className="nav-link" to="/">Home</Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/">Explore</Link>
+            <li className={`nav-item ${mobileMenu}`}>
+              <Link className="nav-link" to="/">Explore</Link>
             </li>
-            <li class = "nav-item">
-              <Link class="nav-link" to="/" id="centerImg"><img src ={img} alt="centerImg"></img></Link>
+            <li className={`nav-item ${mobileMenu}`}>
+              <Link className="nav-link" to="/" id="centerImg"><img src ={img} alt="centerImg"></img></Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link"  to="/dashboard"> Dashboard </Link>
+            <li className={`nav-item ${mobileMenu}`}>
+              <Link className="nav-link"  to="/dashboard"> Dashboard </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link"  to="/connect"> Connect </Link>
+            <li className={`nav-item ${mobileMenu}`}>
+              <Link className="nav-link"  to="/connect"> Connect </Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link logout"  to="/login" id="loginButton" onClick = {logout} >Logout</Link>
+            <li className={`nav-item ${mobileMenu}`}>
+              <Link className="nav-link logout"  to="/login" id="loginButton" onClick = {logout} >Logout</Link>
             </li>
           </>
     }
     else {
         return <>
-        <li class="nav-item">
-          <Link class="nav-link active" to="/about">About</Link>
+        <li className={`nav-item ${mobileMenu}`}>
+          <Link className="nav-link active" to="/about">About</Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/">Home</Link>
+        <li className={`nav-item ${mobileMenu}`}>
+          <Link className="nav-link" to="/">Home</Link>
         </li>
-        <li class = "nav-item">
-          <Link class="nav-link" to="/" id="centerImg"><img src ={img} alt="centerImg"></img></Link>
+        <li className={`nav-item ${mobileMenu}`}>
+          <Link className="nav-link" to="/" id="centerImg"><img src ={img} alt="centerImg"></img></Link>
         </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/">Explore</Link>
+        <li className={`nav-item ${mobileMenu}`}>
+          <Link className="nav-link" to="/">Explore</Link>
         </li>
-        <li class="nav-item">
-            <Link class="nav-link"  to="/login" id="loginButton" >Login</Link>
+        <li className={`nav-item ${mobileMenu}`}>
+            <Link className="nav-link"  to="/login" id="loginButton" >Login</Link>
         </li>
         </>
     }
 }
   return (
     //  Navigation bar
-    <div class = "navbar">
-      <ul class="navbarUl">
+    <div className = {`navbar ${mobileMenu}`}>
+      <ul>
         {navlog()}
       </ul>
-      <div id="centerImg1" ><img src ={img} alt="centerImg"></img></div>
+      <div id="centerImg1" onClick={switchMobile}><img  src ={img} alt="centerImg1"></img></div>
     </div>
     )
 }
