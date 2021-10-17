55a98347268c06cb9d87194de3adf615709874c3
Bara Konateh <bkonateh1@gmail.com>
2021-10-17 20:18:26 +0100

    fix

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase/compat/app'
 import "firebase/compat/auth"
-import 'firebase/database'
+import 'firebase/compat/database'
 
 const fire = firebase.initializeApp({
     apiKey: "AIzaSyBZWWlpZvF9ou5_Zu-nnTpSR6BB6npL8s4",
