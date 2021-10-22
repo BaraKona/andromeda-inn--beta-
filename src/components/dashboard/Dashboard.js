6ff19e2f421731471994ddabf043719b179667c9
Bara Konateh <bkonateh1@gmail.com>
2021-10-22 17:44:55 +0100

    dashboard page init

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect} from "react"
 import { useAuth } from "../../contexts/AuthContext"
-import { useHistory} from "react-router-dom"
+import { useHistory, Link } from "react-router-dom"
+import img from '../../images/icons/magicbook2.svg'
+import wizard from '../../images/icons/wizard.svg'
 import '../css/dashboard.css'
 
 export default function Dashboard() {
@@ -8,29 +10,18 @@ export default function Dashboard() {
   const { currentUser, logout } = useAuth()
   const history = useHistory()
 
-  async function handleLogout() {
-    setError("")
-
-    try {
-      await logout()
-      history.push("/login")
-    } catch {
-      setError("Failed to log out")
-    }
-  }
-
   return (
     <>
       <div>
+        <div className = "sidebar">
+          <div className = "sidebarContainer">
+            <div className= "sidebarIcon"><Link to="/"><img src={img} className="iconImg"></img></Link></div>
+            <div className= "sidebarList"><img src={wizard} className="iconImg"></img></div>
+          </div>
+        </div>
         <div className="dashboard">
           <div className="dashboardContainer">
-            <h1> DASHBOARD in progress...</h1>
           </div>
-          <p className = "errorMsg">{error}</p>
-          {/* <strong>Email:</strong> {currentUser.email} */}
-          {/* <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link> */}
         </div>
       </div>
     </>
--- src/views/dashboardPage.js
+++ src/views/dashboardPage.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import {Switch} from 'react-router-dom'
 import Dashboard from '../components/dashboard/Dashboard.js'
-import Navbar from '../components/layout/navbar'
-// import Footer from "../components/layout/footer.js"
+
 const MainPage = () => {
 
     return(
         <>
-        <Navbar />
         <Switch>
             <section className="main">
                 <Dashboard />
             </section>
         </Switch>
-        {/* <Footer /> */}
         </>
     );
 };
