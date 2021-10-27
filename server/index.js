22e7c73ccced2ca6eb1f890167642e3f48522c78
Bara Konateh <bkonateh1@gmail.com>
2021-10-27 13:21:02 +0100

    backend setup

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlendcoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
