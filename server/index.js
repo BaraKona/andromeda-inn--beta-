2800ec464193eaacc48ee2aff7802fdbb4585603
Bara Konateh <bkonateh1@gmail.com>
2021-11-08 17:40:03 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://andromedainn:andromedainn123@cluster0.ubezw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
