be79cfb13de8d2d418f1f447cfb836fa80a714cc
Bara Konateh <bkonateh1@gmail.com>
2021-12-04 17:14:30 +0000

    errorFix

--- client/src/components/componentSnippets/errorBoundary.js
+++ client/src/components/componentSnippets/errorBoundary.js
@@ -13,7 +13,7 @@ class ErrorBoundary extends React.Component {
 
     componentDidCatch(error, errorInfo) {
       // You can also log the error to an error reporting service
-      logErrorToMyService(error, errorInfo);
+    //   logErrorToMyService(error, errorInfo);
     }
 
     render() {
