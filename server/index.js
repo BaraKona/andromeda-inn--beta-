a91b30e7e6ac619cae0e83fd8ffe497279012786
Bara Konateh <bkonateh1@gmail.com>
2021-12-11 18:20:04 +0000

    heroku

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 import userRoutes from './routes/users.js'
 
 const app = express();
 
+dotenv.config()
 //Need to know what this does
 // support parsing of application/json type post data
 app.use(express.json({ limit: "30mb", extended: true}));
@@ -18,9 +19,13 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://andromedainn:andromedainn123@cluster0.ubezw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+app.get('/', (req, res) => {
+    res.send('Hello andromeda Inn')
+});
+
+// const CONNECTION_URL = 'mongodb+srv://andromedainn:andromedainn123@cluster0.ubezw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
 .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message))
