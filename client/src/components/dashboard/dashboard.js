cd3cf6e8ed7d27d29f72f66e4f087d80da6b8d28
Bara Konateh <bkonateh1@gmail.com>
2021-12-16 11:34:15 +0000

    Update ++
    Added swiper to project
    recent posts now swipes automatically, or dynamically on user input
    Shows up to 8 posts

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import Post from '../post/smallPost'
 import { useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+
+import 'swiper/swiper.scss'; // core Swiper
+import 'swiper/modules/pagination/pagination.scss'; // Pagination module
 import './css/dashboard.scss'
 
 function Dashboard() {
@@ -13,15 +18,37 @@ function Dashboard() {
                 <h2> This page is being worked on </h2>
             </div>
             <div className="dashboardPosts">
-            <h2> Recent Posts</h2>
-            <div className ="recentPosts">
+                <h2> Recent Posts</h2>
+                <div className ="recentPosts">
                     {!posts.length ? <div> Loading... </div> : (
-                        <div className="smallPostMap"> {posts.slice(0, 3).map((post) => (
-                            <div className = "smallPostItem" key={post._id}>
+                        <Swiper className="swiperClass"
+                        modules={[Navigation, Pagination, A11y, Autoplay ]}
+                        pagination={{ "dynamicBullets": true, clickable: true }}
+                        spaceBetween={20}
+                        slidesPerView={2.6}
+                        autoplay={{ "delay": 10000, "disableOnInteraction": true}}
+                        onSlideChange={() => console.log('slide change')}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        breakpoints={{
+                            0: {
+                              width: 0,
+                              slidesPerView: 1,
+                            },
+                            768: {
+                              width: 768,
+                              slidesPerView: 2,
+                            },
+                            1200: {
+                              width: 1200,
+                              slidesPerView: 2.6,
+                            },
+                          }}
+                        > {posts.slice(0, 9).map((post) => (
+                            <SwiperSlide key={post._id}>
                                 <Post post={post}></Post>
-                            </div>
+                            </SwiperSlide>
                         ))}
-                        </div>
+                        </Swiper>
                     )}
                 </div>
             </div>
