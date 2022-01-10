fe920d971064e55d682a45281d39e54db96c14a2
Bara Konateh <bkonateh1@gmail.com>
2022-01-10 16:22:32 +0000

    minor

--- client/src/components/dashboard/projects/projectComponents.js
+++ client/src/components/dashboard/projects/projectComponents.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {book2, settings} from '../../../images/Icon/index'
+import {book2, settings} from '../../../images/Icon'
 import dayjs from 'dayjs'
 import './css/projectComponent.scss'
 
